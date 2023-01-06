ae7408e7100c7182e8aaf199c745d3ecff396a12
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-01-06 15:55:51 +0700

    Edited

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <bits/stdc++.h>
+using namespace std;
+ 
+string printRandomString(){
+   string alphabet[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string res = "";
+    res = alphabet[rand() % 9];
+    return res;
+}
+int main(){
+   cout << "Press Enter 3 times to reveal your future.";
+   for(int i = 0; i < 3; i++){
+        cin.get();
+   }
+   srand(time(0));
+   cout << "You will get " << printRandomString() << " in this 261102.";
+   return 0;
+}
